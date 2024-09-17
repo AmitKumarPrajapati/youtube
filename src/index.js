425eae72c3354db66d455865d6531b21bc93e76e
AmitKumarPrajapati <amitprajapati1993.ap@gmail.com>
2024-09-17 07:23:52 +0530

    added more code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import connectDB from './db/index.js';
 import dotenv from 'dotenv';
+import app from './app.js';
 
 dotenv.config({ path: './env' })
 
-connectDB();
+connectDB()
+.then(()=> {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`Server is running on $${process.env.PORT}}`)
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed!!!!!", err);
+})
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(re,res,next)
+    } catch (error) {
+        res.status(err.code || 500).join({
+            success: false,
+            message: err.message
+        })
+    }
+
+}
+
+export default asyncHandler;
