aafac4d7455c466b702dc81f8a59d3872ce0b714
AmitKumarPrajapati <amitprajapati1993.ap@gmail.com>
2024-09-17 07:34:26 +0530

    more code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import connectDB from './db/index.js';
-import dotenv from 'dotenv';
-import app from './app.js';
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import app from "./app.js";
 
-dotenv.config({ path: './env' })
+dotenv.config({ path: "./env" });
 
 connectDB()
-.then(()=> {
-    app.listen(process.env.PORT || 8000, ()=> {
-        console.log(`Server is running on $${process.env.PORT}}`)
-    })
-})
-.catch((err) => {
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on $${process.env.PORT}}`);
+    });
+  })
+  .catch((err) => {
     console.log("MongoDB connection failed!!!!!", err);
-})
+  });
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,13 +1,12 @@
 const asyncHandler = (fn) => async (req, res, next) => {
-    try {
-        await fn(re,res,next)
-    } catch (error) {
-        res.status(err.code || 500).join({
-            success: false,
-            message: err.message
-        })
-    }
-
-}
+  try {
+    await fn(re, res, next);
+  } catch (error) {
+    res.status(err.code || 500).join({
+      success: false,
+      message: err.message,
+    });
+  }
+};
 
 export default asyncHandler;
