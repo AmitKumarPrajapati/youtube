cc24672451c7d9a8ec9c63387b14e02b851b3a1a
AmitKumarPrajapati <amitprajapati1993.ap@gmail.com>
2024-09-25 14:05:01 +0530

    more code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -111,8 +111,8 @@ const loginUser = asyncHandler(async (req, res) => {
   };
   return res
     .status(200)
-    .cookie("access token", accessToken, options)
-    .cookie("refresh token", refreshToken, options)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
     .json(
       new ApiResponse(
         200,
